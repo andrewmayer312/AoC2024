850c541c150f1d0ad8eca67c473768ed1b90d16e
C00295936@louisiana.edu <V9WiJYniRR6iqqM>
2024-12-15 12:25:01 -0600

    add day 3 part 2 answer

--- Day 3/Day 3-2.py
+++ Day 3/Day 3-2.py
@@ -20,15 +20,13 @@ def processCompletedArray(in_array):
 
     if len(first_arg) > 3 or len(second_arg) > 3:
         return 0
-    
+
     first_arg = int(''.join(first_arg))
     second_arg = int(''.join(second_arg))
     return first_arg * second_arg
 
 
-
 def main():
-
     operation_array = []
 
     result_sum = 0
@@ -36,31 +34,33 @@ def main():
     first_operand_added = False
     second_operand_added = False
 
+    perform_next_intruction = True
+
     with open("input.txt", "r") as file:
-        #reading file by char
+        # reading file by char
         while True:
             in_char = file.read(1)
             print(in_char)
             if not in_char:
                 print("End of file")
                 break
-            #if char is m, check that next char is u
-            if in_char == 'm' and not operation_array:
+            # if char is m, check that next char is u
+            if (in_char == 'm' or in_char == 'd') and not operation_array:
                 operation_array.append(in_char)
                 continue
             elif len(operation_array) != 0:
-                #if char is u, check that previous char is m and if not empty array
+                # if char is u, check that previous char is m and if not empty array
                 if in_char == 'u' and operation_array[-1] == 'm':
                     operation_array.append(in_char)
                     continue
-                #if char is l, check that previous char is u and if not empty array
+                # if char is l, check that previous char is u and if not empty array
                 elif in_char == 'l' and operation_array[-1] == 'u':
                     operation_array.append(in_char)
                     continue
                 elif in_char == '(' and operation_array[-1] == 'l':
                     operation_array.append(in_char)
                     continue
-                #first digit of first arguement 
+                # first digit of first arguement
                 elif in_char.isdigit() and operation_array[-1] == '(':
                     operation_array.append(in_char)
                     first_operand_added = True
@@ -68,7 +68,8 @@ def main():
                 elif in_char.isdigit() and operation_array[-1].isdigit() and first_operand_added == True:
                     operation_array.append(in_char)
                     continue
-                elif in_char == ',' and operation_array[-1].isdigit() and first_operand_added == True and second_operand_added == False:
+                elif in_char == ',' and operation_array[
+                    -1].isdigit() and first_operand_added == True and second_operand_added == False:
                     operation_array.append(in_char)
                     first_operand_added = False
                     continue
@@ -79,24 +80,54 @@ def main():
                 elif in_char.isdigit() and operation_array[-1].isdigit() and second_operand_added == True:
                     operation_array.append(in_char)
                     continue
-                elif in_char == ')' and operation_array[-1].isdigit() and second_operand_added == True:
+                elif in_char == ')' and operation_array[-1].isdigit() and second_operand_added == True and perform_next_intruction == True:
                     operation_array.append(in_char)
-                    #call a function here to process the operation_array and add the result to 
+                    # call a function here to process the operation_array and add the result to
                     x = processCompletedArray(operation_array)
                     print(x)
                     result_sum += x
                     operation_array = []
                     second_operand_added = False
-                    continue   
+                    continue
+                elif in_char == 'o' and operation_array[-1] == 'd':
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == '(' and operation_array[-1] == 'o':
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == 'n' and operation_array[-1] == 'o':
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == "'" and operation_array[-1] == 'n':
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == 't' and operation_array[-1] == "'":
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == '(' and operation_array[-1] == "t":
+                    operation_array.append(in_char)
+                    continue
+                elif in_char == ')' and operation_array[-1] == "(" and operation_array[-2] == 't':
+                    perform_next_intruction = False
+                    operation_array.append(in_char)
+                    print(operation_array)
+                    print('Operation Disabled')
+                    operation_array = []
+                    continue
+                elif in_char == ')' and operation_array[-1] == "(" and operation_array[-2] == 'o':
+                    perform_next_intruction = True
+                    operation_array.append(in_char)
+                    print(operation_array)
+                    print('Operation Enabled')
+                    operation_array = []
+                    continue
                 else:
                     operation_array = []
                     second_operand_added = False
                     first_operand_added = False
                     continue
-        
-    print(result_sum)
 
-           
+    print(result_sum)
 
 
 main()
